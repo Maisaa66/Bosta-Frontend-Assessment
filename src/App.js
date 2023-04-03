4e766d41a33e74fe9381820072bb27b88a2cd7ad
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-03 23:28:31 +0200

    Update logo to use i18next translations and handle language direction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next'
 import Navbar from './component/Navbar/Navbar';
 import Home from './component/Home/Home';
@@ -28,30 +28,13 @@ let theme = createTheme({
 	  },
 	},
   });
-// Contains the value and text for the options
-const languages = [
-	{ value: '', text: "Options" },
-	{ value: 'en', text: "English" },
-	{ value: 'ar', text: "Arabic" }
-]
 
 function App() {
 	
 	const shipmentDetail = useSelector((state)=>state.shipmentDetail);
 
-	// It is a hook imported from 'react-i18next'
-  const { t, i18n } = useTranslation();
-
-	const [lang, setLang] = useState(i18n.language);
-
-	// This function put query that helps to
-	// change the language
-	const handleChange = e => {
-		setLang(e.target.value);
-		let loc = "http://localhost:3000/";
-		window.location.replace(loc + "?lng=" + e.target.value);
-	}
-
+    const { t, i18n } = useTranslation();
+    document.body.dir = i18n.dir();
 	return (
 		<div className="App">
 <ThemeProvider theme={theme}>
