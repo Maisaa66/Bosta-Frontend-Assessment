840a9996efab85521d410f5471a3839dbabc1934
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-04 00:58:16 +0200

    Handle the language direction in home

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import ShipmentDetail from '../ShipmentDetail/ShipmentDetail';
-import { List, ListItem } from '@mui/material';
 import ActivityLog from '../ActivityLog/ActivityLog';
 import ClientDetail from '../ClientDetail/ClientDetail';
+import { useTranslation } from 'react-i18next'
 
 const Home = () => {
+    const { t, i18n } = useTranslation();
+
     return (
         <div className='container mt-5'> 
 
             <div className='row'>
-                <div className='col-12 '>
+                <div className='col-12 ' >
              
-                <ShipmentDetail></ShipmentDetail>
+                <ShipmentDetail ></ShipmentDetail>
                 
                 </div>
             </div>
 
             <div className='row my-5'>
-                <div className='col-md-8 col-sm-12 '>
+                <div className='col-md-8 col-sm-12 '  dir={i18n.dir()}>
              
-                <ActivityLog></ActivityLog>
+                <ActivityLog ></ActivityLog>
                 
                 </div>
 
-                <div className='col-md-4 col-sm-12 mt-sm-5 mt-md-0'>
+                <div className='col-md-4 col-sm-12 mt-sm-5 mt-md-0' dir={i18n.dir()}>
                     <ClientDetail></ClientDetail>
                 </div>
             </div>
