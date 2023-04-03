afaeb00ee738814640bd80751992e6911ac8035a
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-03 04:46:16 +0200

    progress bar UI is done

--- src/ProgressBar/ProgressBar.jsx
+++ src/ProgressBar/ProgressBar.jsx
@@ -6,11 +6,10 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Check from '@mui/icons-material/Check';
-import SettingsIcon from '@mui/icons-material/Settings';
-import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import VideoLabelIcon from '@mui/icons-material/VideoLabel';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import { FaBoxOpen } from "react-icons/fa";
+import HowToRegIcon from '@mui/icons-material/HowToReg';
 
 const QontoConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
@@ -20,18 +19,18 @@ const QontoConnector = styled(StepConnector)(({ theme }) => ({
   },
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      borderColor: '#784af4',
+      borderColor: '#3CCF4E',
     },
   },
   [`&.${stepConnectorClasses.completed}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      borderColor: '#784af4',
+      borderColor: '#3CCF4E',
     },
   },
   [`& .${stepConnectorClasses.line}`]: {
     borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
-    borderTopWidth: 3,
-    borderRadius: 1,
+    borderTopWidth: 5,
+    borderRadius: 3,
   },
 }));
 
@@ -41,10 +40,10 @@ const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
   height: 22,
   alignItems: 'center',
   ...(ownerState.active && {
-    color: '#784af4',
+    color: '#3CCF4E',
   }),
   '& .QontoStepIcon-completedIcon': {
-    color: '#784af4',
+    color: '#3CCF4E',
     zIndex: 1,
     fontSize: 18,
   },
@@ -59,9 +58,10 @@ const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
 function QontoStepIcon(props) {
   const { active, completed, className } = props;
   const icons = {
-    1: <LocalShippingIcon />,
-    2: <GroupAddIcon />,
-    3: <VideoLabelIcon />,
+    1: <FaBoxOpen />,
+    2: <FaBoxOpen />,
+    3: <LocalShippingIcon />,
+    4: <HowToRegIcon />,
   };
   return (
     <QontoStepIconRoot ownerState={{ active }} className={className}>
@@ -89,85 +89,6 @@ QontoStepIcon.propTypes = {
   completed: PropTypes.bool,
 };
 
-const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
-  [`&.${stepConnectorClasses.alternativeLabel}`]: {
-    top: 22,
-  },
-  [`&.${stepConnectorClasses.active}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(0,255,0) 100%,rgb(0,255,0) 100%,rgb(0,255,0) 100%)',
-    },
-  },
-  [`&.${stepConnectorClasses.completed}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(0,0,0) 0%,rgb(0,0,0) 50%,rgb(0,0,0) 100%)',
-    },
-  },
-  [`& .${stepConnectorClasses.line}`]: {
-    height: 3,
-    border: 0,
-    backgroundColor:
-      theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
-    borderRadius: 1,
-  },
-}));
-
-const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
-  zIndex: 1,
-  color: '#fff',
-  width: 50,
-  height: 50,
-  display: 'flex',
-  borderRadius: '50%',
-  justifyContent: 'center',
-  alignItems: 'center',
-  ...(ownerState.active && {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  }),
-  ...(ownerState.completed && {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-  }),
-}));
-
-function ColorlibStepIcon(props) {
-  const { active, completed, className } = props;
-
-  const icons = {
-    1: <SettingsIcon />,
-    2: <GroupAddIcon />,
-    3: <VideoLabelIcon />,
-  };
-
-  return (
-    <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
-      {icons[String(props.icon)]}
-    </ColorlibStepIconRoot>
-  );
-}
-
-ColorlibStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   * @default false
-   */
-  active: PropTypes.bool,
-  className: PropTypes.string,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   * @default false
-   */
-  completed: PropTypes.bool,
-  /**
-   * The label displayed in the step icon.
-   */
-  icon: PropTypes.node,
-};
 
 const steps = ['Order is created', 'Order recieved from the shipper', 'Order is out for delivery',"Order is deliverd"];
 
@@ -181,13 +102,7 @@ export default function ProgressBar() {
           </Step>
         ))}
       </Stepper>
-      {/* <Stepper alternativeLabel activeStep={1} connector={<ColorlibConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper> */}
+
     </Stack>
   );
 }
