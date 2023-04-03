2552b04e902972c207d8ed5638f6dadb9e86689e
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-03 23:27:27 +0200

    Update Language component to use i18next translations

--- src/component/Language/Language.jsx
+++ src/component/Language/Language.jsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { useState } from 'react';
+import { useTranslation } from 'react-i18next'
+import { setLanguage } from '../../Redux Store/languageSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Language(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  let element=""
+  let element="";
+
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -15,64 +18,81 @@ export default function Language(props) {
     setAnchorEl(null);
   };
 
-if(props.mdScreen){
-    element =               <Button
-    color="error"
-    sx={{ borderRadius: "20px" , display:{md:"inline-block", xs:"none"}}}
-    className="mx-5"
-    onClick={handleClick}
-  >
-    ENG
-    <svg
-      width="0.8rem"
-      height="0.8rem"
-      viewBox="0 0 10 6"
-      fill="red"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      focusable="false"
-      className="mx-2"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1.25 0L5 3.6L8.75 0L10 1.2L5 6L0 1.2L1.25 0Z"
-        fill="#e30613"
-      ></path>
-    </svg>
-  </Button>
-}
-else{
-  element=  <Button
-    color="error"
-    sx={{ borderRadius: "20px"}}
-    className="mx-5"
-    onClick={handleClick}
-  >
-    ENG
-    <svg
-      width="0.8rem"
-      height="0.8rem"
-      viewBox="0 0 10 6"
-      fill="red"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      focusable="false"
-      className="mx-2"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1.25 0L5 3.6L8.75 0L10 1.2L5 6L0 1.2L1.25 0Z"
-        fill="#e30613"
-      ></path>
-    </svg>
-  </Button>
-}
+  const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
+
+  const handleChange = (lng) => {
+    dispatch(setLanguage(lng));
+    console.log(lng);
+    i18n.changeLanguage(lng);
+    setAnchorEl(null);
+    localStorage.setItem('language', lng);
+  };
+  const language = useSelector((state) => state.language);
+  console.log(language);
+
+  if(props.mdScreen){
+    element = (
+      <Button
+        color="error"
+        sx={{ borderRadius: "20px" , display:{md:"inline-block", xs:"none"}}}
+        className="mx-5"
+        onClick={handleClick}
+      >
+        {t('language')}
+        <svg
+          width="0.8rem"
+          height="0.8rem"
+          viewBox="0 0 10 6"
+          fill="red"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          className="mx-2"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1.25 0L5 3.6L8.75 0L10 1.2L5 6L0 1.2L1.25 0Z"
+            fill="#e30613"
+          ></path>
+        </svg>
+      </Button>
+    );
+  }
+  else {
+    element = (
+      <Button
+        color="error"
+        sx={{ borderRadius: "20px"}}
+        className="mx-5"
+        onClick={handleClick}
+      >
+        {t('language')}
+        <svg
+          width="0.8rem"
+          height="0.8rem"
+          viewBox="0 0 10 6"
+          fill="red"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          className="mx-2"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1.25 0L5 3.6L8.75 0L10 1.2L5 6L0 1.2L1.25 0Z"
+            fill="#e30613"
+          ></path>
+        </svg>
+      </Button>
+    );
+  }
+
   return (
     <div>
-        
-        {element}
+      {element}
 
       <Menu
         id="basic-menu"
@@ -83,8 +103,8 @@ else{
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem onClick={handleClose}>English</MenuItem>
-        <MenuItem onClick={handleClose}>Arabic</MenuItem>
+        <MenuItem onClick={() => handleChange('en')}>{t('english')}</MenuItem>
+        <MenuItem onClick={() => handleChange('ar')}>{t('arabic')}</MenuItem>
       </Menu>
     </div>
   );
